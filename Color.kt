33b9f7775ec004737ffcb7d08e77682274bfaed4
Andres203-dot <jesusbufa97@gmail.com>
2026-04-24 16:38:01 -0500

    Add Color.kt for light and dark theme color schemes

---
+++ Color.kt
@@ -0,0 +1,55 @@
+package com.smartlens.actividad3.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Scheme (Sophisticated Deep Blue & Slate)
+val PrimaryLight = Color(0xFF005AC1) // Professional Royal Blue
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD8E2FF)
+val OnPrimaryContainerLight = Color(0xFF001A41)
+
+val SecondaryLight = Color(0xFF575E71) // Slate Blue-Grey
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFDBE2F9)
+val OnSecondaryContainerLight = Color(0xFF141B2C)
+
+val TertiaryLight = Color(0xFF715573) // Sophisticated Muted Plum
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFFBD7FC)
+val OnTertiaryContainerLight = Color(0xFF29132D)
+
+val ErrorLight = Color(0xFFBA1A1A)
+val OnErrorLight = Color(0xFFFFFFFF)
+val ErrorContainerLight = Color(0xFFFFDAD6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+val BackgroundLight = Color(0xFFF8F9FF) // Soft off-white with blue tint
+val OnBackgroundLight = Color(0xFF1A1C1E)
+val SurfaceLight = Color(0xFFF8F9FF)
+val OnSurfaceLight = Color(0xFF1A1C1E)
+
+// Dark Scheme (Deep Professional Dark Mode)
+val PrimaryDark = Color(0xFFAEC6FF)
+val OnPrimaryDark = Color(0xFF002E6C)
+val PrimaryContainerDark = Color(0xFF004396)
+val OnPrimaryContainerDark = Color(0xFFD8E2FF)
+
+val SecondaryDark = Color(0xFFBFC6DC)
+val OnSecondaryDark = Color(0xFF293041)
+val SecondaryContainerDark = Color(0xFF3F4759)
+val OnSecondaryContainerDark = Color(0xFFDBE2F9)
+
+val TertiaryDark = Color(0xFFDEBCDF)
+val OnTertiaryDark = Color(0xFF402843)
+val TertiaryContainerDark = Color(0xFF583E5B)
+val OnTertiaryContainerDark = Color(0xFFFBD7FC)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000A)
+val OnErrorContainerDark = Color(0xFFFFDAD6)
+
+val BackgroundDark = Color(0xFF111318) // Very dark professional blue-black
+val OnBackgroundDark = Color(0xFFE2E2E6)
+val SurfaceDark = Color(0xFF111318)
+val OnSurfaceDark = Color(0xFFE2E2E6)
